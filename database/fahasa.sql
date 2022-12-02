8b62aad01a2a009a20a562ba60bdb69487579bf0
Nguyễn Phan Anh Tuấn <tuan.nguyenphananh@gmail.com>
2022-12-03 06:57:10 +0700

    Update fahasa.sql

--- database/fahasa.sql
+++ database/fahasa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2022 at 10:27 AM
+-- Generation Time: Dec 03, 2022 at 12:56 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -45,7 +45,7 @@ CREATE TABLE `addresses` (
 
 INSERT INTO `addresses` (`id`, `name`, `phone_number`, `address`, `city`, `district`, `ward`, `is_default`, `user_id`) VALUES
 (1, 'Kiu', '0941974458', '221 Lê Văn Việt', 'Tỉnh Hưng Yên', 'Thành phố Hưng Yên', 'Xã Tân Hưng', 1, 1),
-(3, 'Nguyễn Phan Anh Tuấn', '0941974458', '691 Lê Văn Việt', 'Tỉnh Bắc Ninh', 'Thành phố Bắc Ninh', 'Phường Khắc Niệm', 0, 1),
+(3, 'Nguyễn Phan Anh Tuấn', '0941974458', '691 Lê Văn Việt', 'Thành phố Hồ Chí Minh', 'Quận 9', 'Phường Long Thạnh Mỹ', 0, 1),
 (5, 'Anh Tuan', '2321312312', 'Lô 34 đường số 10 KĐT Lê Hồng Phong', 'Tỉnh Khánh Hòa', 'Thành phố Nha Trang', 'Phường Phước Hải', 0, 1),
 (6, 'Nguyễn Phan Anh Tuấn', '0838940955', '580AB Nguyen Oanh', 'Thành phố Hồ Chí Minh', 'Quận Gò Vấp', 'Phường 17', 1, 6),
 (7, 'Ho', '8584449394', '149X/23 Tô Hiến Thành', 'Thành phố Hồ Chí Minh', 'Quận 11', 'Phường 05', 0, 6);
@@ -87,7 +87,10 @@ INSERT INTO `books` (`id`, `title`, `author`, `description`, `image`, `price`, `
 (8, 'Thay Đổi Cuộc Sống Với Nhân Số Học', 'Lê Đỗ Quỳnh Hương', 'Đầu năm 2020, chuỗi chương trình “Thay đổi cuộc sống với Nhân số học” của  biên tập viên, người dẫn chương trình quen thuộc tại Việt Nam Lê Đỗ Quỳnh Hương ra đời trên Youtube, với mục đích chia sẻ kiến thức, giúp mọi người tìm hiểu và phát triển, hoàn thiện bản thân, các mối quan hệ xã hội thông qua bộ môn Nhân số học. Chương trình đã nhận được sự yêu mến và phản hồi tích cực của rất nhiều khán giả và độc giả sau mỗi tập phát sóng.', 'https://cdn0.fahasa.com/media/catalog/product/t/d/tdcsvnsh.jpg', 248000, 29, 2, 1, 3, 51, '2016-02-24 17:00:00', 416, '8935086853634', '2022-10-09 09:35:46', '2022-10-30 09:08:48'),
 (9, 'Hiểu Về Trái Tim (Tái Bản 2019)', 'Minh Niệm', '“Hiểu về trái tim” là một cuốn sách đặc biệt được viết bởi thiền sư Minh Niệm. Với phong thái và lối hành văn gần gũi với những sinh hoạt của người Việt, thầy Minh Niệm đã thật sự thổi hồn Việt vào cuốn sách nhỏ này.', 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_195509_1_14922.jpg', 138000, 9, 3, 1, 1, 3354343, '2022-01-26 17:00:00', 480, '8935086849903', '2022-10-09 17:24:31', '2022-10-30 09:42:35'),
 (10, 'Rèn Luyện Tư Duy Phản Biện', 'Albert Rutherford', 'Như bạn có thể thấy, chìa khóa để trở thành một người có tư duy phản biện tốt chính là sự tự nhận thức. Bạn cần phải đánh giá trung thực những điều trước đây bạn nghĩ là đúng, cũng như quá trình suy nghĩ đã dẫn bạn tới những kết luận đó. Nếu bạn không có những lý lẽ hợp lý, hoặc nếu suy nghĩ của bạn bị ảnh hưởng bởi những kinh nghiệm và cảm xúc, thì lúc đó hãy cân nhắc sử dụng tư duy phản biện! Bạn cần phải nhận ra được rằng con người, kể từ khi sinh ra, rất giỏi việc đưa ra những lý do lý giải cho những suy nghĩ khiếm khuyết của mình. Nếu bạn đang có những kết luận sai lệch này thì có một sự thật là những đức tin của bạn thường mâu thuẫn với nhau và đó thường là kết quả của thiên kiến xác nhận, nhưng nếu bạn biết điều này, thì bạn đã tiến gần hơn tới sự thật rồi!', 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_195509_1_18448.jpg', 99000, 34, 4, 1, 1, 123, '2017-01-11 17:00:00', 208, '8936066687812', '2022-10-09 17:34:38', '2022-10-09 17:34:38'),
-(11, 'Càng Kỷ Luật, Càng Tự Do', 'Ca Tây', 'KỶ LUẬT vốn là ván cờ bạn phải tự đấu với chính mình. Thắng - bạn sẽ có được “bản năng của người mạnh mẽ nhất”, đó là sự tự kiểm soát bản thân. Thua - bạn mãi sống trong cảm giác tạm bợ, nuối tiếc. Càng dễ dàng dung túng cho những thói quen trì hoãn bao nhiêu, cuộc sống của bạn sẽ đi càng nhanh tới sự mất kiểm soát và thiếu quy hoạch bấy nhiêu.', 'https://cdn0.fahasa.com/media/catalog/product/c/o/cover_c_ng-k_-lu_t_-c_ng-t_-do.jpg', 119000, 36, 4, 5, 3, 188, '2022-05-09 17:00:00', 288, '8936186546945', '2022-10-26 11:53:57', '2022-10-26 11:53:57');
+(11, 'Càng Kỷ Luật, Càng Tự Do', 'Ca Tây', 'KỶ LUẬT vốn là ván cờ bạn phải tự đấu với chính mình. Thắng - bạn sẽ có được “bản năng của người mạnh mẽ nhất”, đó là sự tự kiểm soát bản thân. Thua - bạn mãi sống trong cảm giác tạm bợ, nuối tiếc. Càng dễ dàng dung túng cho những thói quen trì hoãn bao nhiêu, cuộc sống của bạn sẽ đi càng nhanh tới sự mất kiểm soát và thiếu quy hoạch bấy nhiêu.', 'https://cdn0.fahasa.com/media/catalog/product/c/o/cover_c_ng-k_-lu_t_-c_ng-t_-do.jpg', 119000, 36, 4, 5, 3, 188, '2022-05-09 17:00:00', 288, '8936186546945', '2022-10-26 11:53:57', '2022-10-26 11:53:57'),
+(14, 'Muôn Kiếp Nhân Sinh', 'Nguyên Phong', '“Muôn kiếp nhân sinh” là tác phẩm do Giáo sư John Vũ - Nguyên Phong viết từ năm 2017 và hoàn tất đầu năm 2020 ghi lại những câu chuyện, trải nghiệm tiền kiếp kỳ lạ từ nhiều kiếp sống của người bạn tâm giao lâu năm, ông Thomas – một nhà kinh doanh tài chính nổi tiếng ở New York. Những câu chuyện chưa từng tiết lộ này sẽ giúp mọi người trên thế giới chiêm nghiệm, khám phá các quy luật về luật Nhân quả và Luân hồi của vũ trụ giữa lúc trái đất đang gặp nhiều tai ương, biến động, khủng hoảng từng ngày.', 'https://cdn0.fahasa.com/media/catalog/product/m/u/muonkiepnhansinh.jpg', 168000, 36, 2, 5, 1, 200, '0000-00-00 00:00:00', 408, '8935086851760', NULL, NULL),
+(15, 'Hướng Dẫn Kiếm Tiền Trên Tiktok', 'Giang Trung Nguyên', 'TikTok là một phần mềm giao tiếp xã hội để tạo nhạc và có thể quay các video ngắn, đồng thời, đây cũng là cộng đồng video có độ dài 15 giây chuyên dành cho giới trẻ. Fan có thể lựa chọn ca khúc và quay một video ngắn 15 giây thông qua phần mềm này và tạo ra tác phẩm của chính mình. Không ít cá nhân thu được lợi tức trên TikTok. Ngoài ra, cũng có không ít công ty cũng xem TikTok như một địa điểm marketing mới của mình.', 'https://cdn0.fahasa.com/media/catalog/product/i/m/image_244718_1_1015.jpg', 189000, 20, 3, 3, 3, 200, '2020-12-06 17:00:00', 424, '8936066691260', NULL, NULL),
+(16, 'Ba Bước Đến Vàng', 'Sharon L, Lechter, Greg S, Reid', 'Ba bước đến vàng chia sẻ những tinh hoa và tư vấn từ rất nhiều con người phi thường, những người đã kiên trì vững bước trong những thời điểm khó khăn và trở thành hình mẫu cho tất cả chúng ta. Câu chuyện này được xây dựng dựa trên trải nghiệm thực tế của tác giả khi khám phá ra triết lý của Napoleon Hill đã truyền cảm hứng lớn lao cho thành công ngoài sức tưởng tượng của các nhà quản lý, nhà nhân đạo, vận động viên và doanh nhân. Tác giả đã viết ra một câu chuyện hấp dẫn; ngoại trừ ba nhân vật Mia, David và Jonathan Buckland, tất cả các nhân vật trong cuốn sách này đều có thật và chính tác giả có đặc ân là được nói chuyện trực tiếp với họ. Dũng khí và thành tựu của họ là có thật. Những bài học cuộc sống mà họ đưa ra là chân thành. Bố cục câu chuyện là những lời của chính Napoleon Hill đã viết ra vào đầu thế kỉ 20. Cho đến tận ngày hôm nay, những lời này vẫn còn mạnh mẽ như khi chúng được viết ra lần đầu tiên.\r\n\r\nMã hàng	8935210302045\r\nTên Nhà Cung Cấp	Tân Việt\r\nTác giả	Sharon L, Lechter, Greg S, Reid\r\nNgười Dịch	Nghi Phương\r\nNXB	Dân Trí\r\nNăm XB	2022\r\nTrọng lượng (gr)	420\r\nKích Thước Bao Bì	20.5 x 14.5 cm\r\nSố trang	342\r\nHình thức	Bìa Mềm\r\nSản phẩm hiển thị trong	\r\nTân Việt\r\nRƯỚC DEAL LINH ĐÌNH VUI ĐÓN TRUNG THU\r\nSản phẩm bán chạy nhất	Top 100 sản phẩm Khởi Nghiệp - Làm Giàu bán chạy của tháng\r\nGiá sản phẩm trên Fahasa.com đã bao gồm thuế theo luật hiện hành. Bên cạnh đó, tuỳ vào loại sản phẩm, hình thức và địa chỉ giao hàng mà có thể phát sinh thêm chi phí khác như Phụ phí đóng gói, phí vận chuyển, phụ phí hàng cồng kềnh,...\r\nBa bước đến vàng chia sẻ những tinh hoa và tư vấn từ rất nhiều con người phi thường, những người đã kiên trì vững bước trong những thời điểm khó khăn và trở thành hình mẫu cho tất cả chúng ta. Câu chuyện này được xây dựng dựa trên trải nghiệm thực tế của tác giả khi khám phá ra triết lý của Napoleon Hill đã truyền cảm hứng lớn lao cho thành công ngoài sức tưởng tượng của các nhà quản lý, nhà nhân đạo, vận động viên và doanh nhân. Tác giả đã viết ra một câu chuyện hấp dẫn; ngoại trừ ba nhân vật Mia, David và Jonathan Buckland, tất cả các nhân vật trong cuốn sách này đều có thật và chính tác giả có đặc ân là được nói chuyện trực tiếp với họ. Dũng khí và thành tựu của họ là có thật. Những bài học cuộc sống mà họ đưa ra là chân thành. Bố cục câu chuyện là những lời của chính Napoleon Hill đã viết ra vào đầu thế kỉ 20. Cho đến tận ngày hôm nay, những lời này vẫn còn mạnh mẽ như khi chúng được viết ra lần đầu tiên.', 'https://cdn0.fahasa.com/media/catalog/product/8/9/8935210302045.jpg', 145000, 20, 3, 3, 3, 200, '2021-01-03 17:00:00', 342, '8935210302045', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -136,7 +139,8 @@ CREATE TABLE `orders` (
 INSERT INTO `orders` (`id`, `name`, `phone_number`, `address`, `city`, `district`, `ward`, `total`, `status`, `user_id`, `created_at`) VALUES
 (11, 'Nguyễn Phan Anh Tuấn', '0941974458', '691 Lê Văn Việt', 'Tỉnh Bắc Ninh', 'Thành phố Bắc Ninh', 'Phường Khắc Niệm', 197320, 0, 1, '2022-11-30 09:00:31'),
 (12, 'Anh Tuan', '2321312312', 'Lô 34 đường số 10 KĐT Lê Hồng Phong', 'Tỉnh Khánh Hòa', 'Thành phố Nha Trang', 'Phường Phước Hải', 376740, 0, 1, '2022-11-30 09:01:35'),
-(13, 'Kiu', '0941974458', '221 Lê Văn Việt', 'Tỉnh Hưng Yên', 'Thành phố Hưng Yên', 'Xã Tân Hưng', 197320, 0, 1, '2022-11-30 09:25:53');
+(13, 'Kiu', '0941974458', '221 Lê Văn Việt', 'Tỉnh Hưng Yên', 'Thành phố Hưng Yên', 'Xã Tân Hưng', 197320, 0, 1, '2022-11-30 09:25:53'),
+(14, 'Anh Tuan', '2321312312', 'Lô 34 đường số 10 KĐT Lê Hồng Phong', 'Tỉnh Khánh Hòa', 'Thành phố Nha Trang', 'Phường Phước Hải', 90000, 0, 1, '2022-11-30 17:11:47');
 
 -- --------------------------------------------------------
 
@@ -160,7 +164,8 @@ INSERT INTO `order_details` (`id`, `order_id`, `book_id`, `quantity`) VALUES
 (9, 11, 2, 1),
 (10, 12, 9, 3),
 (11, 13, 11, 2),
-(12, 13, 2, 1);
+(12, 13, 2, 1),
+(13, 14, 2, 2);
 
 -- --------------------------------------------------------
 
@@ -222,7 +227,8 @@ INSERT INTO `ratings` (`id`, `book_id`, `user_id`, `rating`, `comment`, `created
 (15, 11, 5, 5, 'Hiện nay nhiều người đang đổ xô đi đầu tư vào bitcoin, ethereum... mà không chịu tìm hiểu bản chất của blockchain (blockchain là công nghệ quyết định sự hoạt động của tiền ảo). Đọc cuốn này sẽ giúp bạn có được cái nhìn cơ bản nhất. Sách khá dễ hiểu.', '2022-11-15'),
 (21, 2, 8, 4, 'Cũng được đấy', '2022-11-25'),
 (22, 3, 8, 4, 'Giao hàng nhanh chóng mặt ???? Nội dung sách hay - trình bày các vấn đề vô cùng đơn giản dễ hiểu, k mang nặng tính hàn lâm, kỹ thuật. Chất lượng in ấn tốt. Nhưng phần dịch thuật chưa thật sự tốt.', '2022-11-25'),
-(23, 9, 5, 5, 'Một khi mở cửa trái tim của con người ra. Ta đ*o thấy gì', '2022-11-26');
+(23, 9, 5, 5, 'Một khi mở cửa trái tim của con người ra. Ta đ*o thấy gì', '2022-11-26'),
+(24, 6, 1, 4, 'eoqfk;qekq\n', '2022-12-02');
 
 -- --------------------------------------------------------
 
@@ -353,13 +359,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `addresses`
 --
 ALTER TABLE `addresses`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `books`
 --
 ALTER TABLE `books`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `categories`
@@ -371,13 +377,13 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `publishers`
@@ -389,7 +395,7 @@ ALTER TABLE `publishers`
 -- AUTO_INCREMENT for table `ratings`
 --
 ALTER TABLE `ratings`
-  MODIFY `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` bigint(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `suppliers`
